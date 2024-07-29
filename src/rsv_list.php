e557e501c38e56f374d1f03c5b28487e590417e1
Iori Ide <k23gjk03@st.kyusan-u.ac.jp>
2024-07-29 18:49:39 +0900

    add file

---
+++ src/rsv_list.php
@@ -0,0 +1,55 @@
+<?php
+namespace aic;
+
+use aic\models\Reserve;
+use aic\models\User;
+use aic\models\KsuCode;
+use aic\models\Util;
+
+use aic\views\Html;
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1; 
+
+echo '<h3>申請状況一覧</h3>' . PHP_EOL;
+include 'include/_rsv_search.inc.php';
+// pagination on top
+$num_rows = (new Reserve)->getNumRows($inst_id, $date1, $date2, $status);
+echo Html::pagination($num_rows, KsuCode::PAGE_ROWS, $page);
+
+echo '<table class="table table-hover">'. PHP_EOL;
+echo '<tr><th>予約番号</th><th>部屋No.</th><th>利用機器名</th><th>利用目的</th><th>利用予定日</th>
+  <th>利用時間帯</th><th>利用責任者</th><th>承認状態</th><th>操　作</th></tr>'. PHP_EOL;
+
+$rows= (new Reserve)->getListByInst($inst_id, $date1, $date2, $status, $page);
+foreach ($rows as $row){ //予約テーブルにある予約の数だけ繰り返す
+  echo '<tr>'. 
+  '<td>' . $row['code'] . '</td>' . PHP_EOL . 
+  '<td>' . $row['room_no'] . '</td>' . PHP_EOL . 
+    //'<td>' . Util::jpdate($row['reserved']) . '</td>' . PHP_EOL .  //申請日時を表示
+    //'<td>' . $row['apply_name'] . '</td>' . PHP_EOL . //申請者氏名を表示
+    //'<td>' . $row['fullname'] . '</td>' . PHP_EOL . //利用機器名を表示
+    '<td>' . $row['shortname'] . '</td>' . PHP_EOL . //利用機器名(省略)を表示
+    '<td>' . KsuCode::RSV_PURPOSE[$row['purpose_id']] .' ' . $row['purpose'] . '</td>' . PHP_EOL;
+  $date1 = Util::jpdate($row['stime']) ;
+  $date2 = Util::jpdate($row['etime']) ;
+  echo '<td>' . $date1 . '</td>' . PHP_EOL; //利用日を表示
+  $time2 = ($date1==$date2) ? substr($row['etime'], 10,6) : '';//日をまかがった予約は終了時刻表示なし
+  echo '<td>' . substr($row['stime'], 10,6) . '～' . $time2 . '</td>'; //利用時間帯を表示
+  echo '<td>' . $row['master_name'] . '</td>';//利用責任者者氏名を表示
+  $i = $row['process_status'];
+  echo '<td>' . $rsv_status[$i] . '</td>';//申請状態を表示
+  $rsv_id = $row['id'];
+  $status = $row['process_status'];
+  $label = ($status==1 or $status==3) ? '承認' : '却下';
+  echo '<td>';
+  $is_admin = (new User)->isAdmin();
+  if ($is_admin){
+    echo '<a class="btn btn-sm btn-outline-info" href="?do=rsv_grant&id='.$rsv_id.'">'.$label.'</a>' . PHP_EOL;
+  }
+  echo '<a class="btn btn-sm btn-outline-success" href="?do=rsv_detail&id='.$row['id'].'">詳細</a>' .
+    '</td></tr>' . PHP_EOL;
+}
+echo '</table>';
+
+// pagination at bottom
+echo Html::pagination($num_rows, KsuCode::PAGE_ROWS, $page);
---
+++ src/rsv_save.php
@@ -0,0 +1,88 @@
+<?php
+// namespace aic;
+
+use aic\models\Reserve;
+use aic\models\Member;
+use aic\models\User;
+use aic\models\RsvMember;
+use aic\models\RsvSample;
+use aic\models\Util;
+
+$data = $_POST;
+$rsv_id = $data['id'];
+$rsv = [
+    'id'=>0, 'code'=>'', 'instrument_id'=>0, 'apply_mid'=>0, 'master_mid'=>0,'process_status'=>1,'purpose_id'=>0,
+    'purpose'=>'','other_num'=>0, 'other_user'=>'', 'stime'=>'','etime'=>'','sample_name'=>'','sample_state'=>1,
+    'xray_chk'=>0, 'xray_num'=>'', 'memo'=>'',
+];
+foreach($rsv as $key=>$val){
+    if (array_key_exists($key, $data)){
+        $rsv[$key] = $data[$key];
+    }
+}
+
+$errors = [];
+$existed_rsv = (new Reserve)->getListByInst($rsv['instrument_id'], $rsv['stime'], $rsv['etime']);
+if (count($existed_rsv) > 0 ){
+    $errors[] = sprintf("ほかの予約時間帯と被っています：%s～%s：", Util::jpdate($rsv['stime'],true), Util::jpdate($rsv['etime'],true));
+}
+
+$rsv['master_mid'] = (new User)->getLoginMid();
+$member = (new Member)->getDetailBySid($data['master_sid']);
+if ($member){
+    $rsv['master_mid'] = $member['id'];
+}
+
+$rsv_members = [];
+foreach ($data['rsv_member'] as $sid){
+    if (empty($sid)) continue;
+    $member = (new Member)->getDetailBySid($sid);
+    if ($member){
+        $rsv_members[] = $member;
+    }else{
+        $errors[] = sprintf("'%s'：無効な利用代表者IDです", $sid);
+    }
+}
+if (count($rsv_members) == 0){
+    $errors[] = "有効な利用代表者が指定されていません";
+}
+if (count($errors) > 0){
+    echo '<h3 class="text-danger">以下の理由により登録できません</h3>' . PHP_EOL;
+    echo '<ul class="list-group">';
+    foreach ($errors as $error){
+        echo '<li class="list-group-item list-group-item-info">エラー：' . $error . '！</li>' . PHP_EOL;
+    }
+    echo '</ul>';
+    echo '<button class="btn btn-primary m-2" onclick="history.back();">戻る</button>';
+}else{
+    if ($rsv_id == 0){
+        $rsv['code'] = (new Reserve)->nextCode();
+        $rsv['reserved'] = date('Y-m-d H:i:s');
+    }
+    $id = (new Reserve)->write($rsv);
+
+    if ($rsv_id == 0){
+        $rsv_id = $id;
+    }
+
+    (new RsvMember)->reset($rsv_id);
+    (new RsvSample)->reset($rsv_id);
+
+    foreach ($data['rsv_member'] as $sid){
+        if (empty($sid)) continue;
+        $member = (new Member)->getDetailBySid($sid);
+        if ($member){
+            $record = ['id'=>0, 'reserve_id'=>$rsv_id, 'member_id'=>$member['id']];
+            $rs = (new RsvMember)->write($record);
+        }
+    }
+
+    foreach ($data['rsv_sample'] as $val){
+        $other = $val==4 ?$other = $data['sample_other'] : '';
+        $record = ['id'=>0, 'reserve_id'=>$rsv_id, 'nature'=>$val, 'other'=>$other];
+        // echo '<pre>'; print_r($record); echo '</pre>';
+        $rs = (new RsvSample)->write($record);
+    }
+
+    header('Location:?do=rsv_detail&id='.$rsv_id);
+}
